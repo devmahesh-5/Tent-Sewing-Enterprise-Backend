4683928c623c9a0e92fcf1b9b48aec78e96a6dfc
Mahesh Bhandari <n31mahesh@gmail.com>
2025-01-21 16:03:16 +0545

    App router configuration

--- src/app.js
+++ src/app.js
@@ -22,4 +22,14 @@ app.use(express.urlencoded(
 app.use(express.json({limit:"16kb"}))
 app.use(cookieParser());//to parse cookies
 
+    //import routes
+import userRouter from "./routes/user.routes.js";
+import productRouter from "./routes/product.routes.js";
+
+
+//user routes
+app.use("/api/v1/users",userRouter);
+
+//product routes
+app.use("/api/v1/products",productRouter);
 export default app
